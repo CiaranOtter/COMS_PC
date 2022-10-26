ec9e594a2c5e1baba1255f123b9d96e36edd2d2e
ciaran <ciaranjotter@gmail.com>
2022-10-26 18:00:18 +0200

    bug fix

--- Assignment/scan/scan.cpp
+++ Assignment/scan/scan.cpp
@@ -2,28 +2,28 @@
 
 using namespace std;
 
-void scan(out[], in[]);
-void printlist(out[]);
+int * scan(int *out[], int in[], int l);
+void printlist(int out[]);
 
 int main() {
 
     int set[8] = {2,3,7,7,10,17,23,26};
-    int *setResult[8];
 
     printList(set);
-    scan(setResults, set);
+    int setResult[8] = scan(set, 8);
     printList(setResult);
 
     return 0;
 }
 
-void scan(int *out[], int in[]) {
+int * scan(int in[], int l) {
     int i = 0;
-    int N = out->size();
-    *out[0] = in[0];
-    for (i = 1; i < N; i++) {
-        *out[i] = in[i] + *out[i-1];
+    int out[l];
+    out[0] = in[0];
+    for (i = 1; i < l; i++) {
+        out[i] = in[i] + out[i-1];
     }
+    return out;
 }
 
 void printList(int out[]) {
